b53d8bc8574c0196085353e8de6406413062dddb
Frankie Homewood <fhomewood98@gmail.com>
2022-07-12 23:19:28 +0100

    complete feature box scroll feature, removed overscrolling and resets scroll across panes

--- sheet_variables.py
+++ sheet_variables.py
@@ -440,14 +440,16 @@ def neatify_string(context,string, splitlen=48):
 
 def feature_box_select(context, line_index):
 	def func(context):
+		current_headings = list(context.get_current_feature().keys())
+		scroll_offset = context.art_blocks["Features Box"].scroll
 		if mouse_event(context) == "Double Left Click":
-			scroll_offset = context.art_blocks["Features Box"].scroll
-			current_headings = list(context.get_current_feature().keys())
+
 			if line_index < len(current_headings)-scroll_offset:
 				selected_heading = list(context.get_current_feature().keys())[line_index+scroll_offset]
 				selected_attribute = context.get_current_feature()[selected_heading]
 				if type(selected_attribute) == dict:
 					context.feature_box_keys.append(selected_heading)
+					context.art_blocks["Features Box"].scroll = 0
 				elif type(selected_attribute) == str:
 					string = selected_attribute
 					popup_text = f""" 
@@ -488,27 +490,14 @@ def feature_box_select(context, line_index):
 					activate_popup(context,popup_text)
 		if mouse_event(context) in ["Right Click", "Double Right Click"]:
 			context.feature_box_keys = context.feature_box_keys[:-1]
+			context.art_blocks["Features Box"].scroll = 0
 		if mouse_event(context) == "Scroll Up":
 			context.art_blocks["Features Box"].scroll = max(0, context.art_blocks["Features Box"].scroll - 1)
 		if mouse_event(context) == "Scroll Down":
-			context.art_blocks["Features Box"].scroll += 1
-		update_features(context)
+			context.art_blocks["Features Box"].scroll = min(context.art_blocks["Features Box"].scroll+1, len(current_headings)-1)
 
 	return func
 
-def update_features(context):
-	pass
-	# text = context.art_blocks["Popup"].text
-	# scroll_offset = context.art_blocks["Popup"].scroll
-	# temp_var_array = [[0,50,f'{scroll_offset}']]
-	# line_number = 0
-	# for paragraph in text.split('\n'):
-	# 	for chunk in [paragraph[i:i+49] for i in range(0,len(paragraph), 49)]:
-	# 		temp_var_array += [[line_number - scroll_offset + 20,16,chunk]]
-	# 		line_number+=1
-	# scroll_offset = min(len(temp_var_array),scroll_offset)
-	# context.art_blocks["Popup"].var_array = temp_var_array[scroll_offset+1:scroll_offset+20]
-	
 def roll(context,n,k,b):
 	def func(context):
 		if mouse_event(context) =="Double Left Click":
