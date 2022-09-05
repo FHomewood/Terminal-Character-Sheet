a35e5c6a1a5beb006e0f4607659e8bd05ce915c4
Frankie Homewood <fhomewood98@gmail.com>
2022-09-05 19:44:50 +0100

    updated example_character.py with accurate example

--- example_character.py
+++ example_character.py
@@ -1,78 +1,50 @@
 import TerminalCharacterSheet as tcs
 
+def roll_stats(c):
+    c.strength = 9
+    c.dexterity = 16
+    c.constitution = 16
+    c.intelligence = 13
+    c.wisdom = 14
+    c.charisma = 17
 
 def set_up(context):
     c = context.character
 
+    roll_stats(c)
+    sheet_layout(c)
+
+
     c.name = "Corrin TossCobble"
-    c.alignment = "Lawful Neutral"
-    c.race = "Rock Gnome"
-    c.speed = 30
+    c.alignment = "Lawful Good"
+    c.gender = 'Male'
+
+
+    c.race = "Lightfoot Halfling"
+    c.size = 'Small (3\'½\")'
+    c.weight = 'Light (44lbs)'
+    c.age = 25
+    c.speed = 25
+    c.dexterity += 2
+    c.charisma += 1
+    c.features["Proficiencies"]["Languages"]["Common"] = """"""
+    c.features["Proficiencies"]["Languages"]["Halfling"] = """"""
+    c.features["Features & Traits"]["Racial Traits"]["Lucky"] = \
+        """When you roll a 1 on the d20 for an attack roll, ability check, or saving throw, you can reroll the die and must use the new roll."""
+    c.features["Features & Traits"]["Racial Traits"]["Brave"] = \
+        """You have advantage on saving throws against being frightened."""
+    c.features["Features & Traits"]["Racial Traits"]["Halfling Nimbleness"] = \
+        """You can move through the space of any creature that is of a size larger than yours."""
+    c.features["Features & Traits"]["Racial Traits"]["Naturally Stealthy"] = \
+        """You can attempt to hide even when you are obscured only by a creature that is at least one size larger than you."""
+
 
     c.character_class = "Bard"
     c.level = 1
-
     c.hit_dice = '1d8'
     c.proficiency_bonus = 2
     c.saving_throws = ["Dexterity", "Charisma"]
-
-    c.strength = 8
-    c.dexterity = 14
-    c.constitution = 13
-    c.intelligence = 15
-    c.wisdom = 10
-    c.charisma = 15
-
-    c.skill_proficiencies = [
-        'Acrobatics', 'Deception', 'Perception',
-        'Performance', 'Sleight of Hand'
-    ]
-
-    c.features = \
-        {
-            "Proficiencies": {
-                "Weapons & Armour": {},
-                "Tools": {},
-                "Languages": {}
-            },
-            "Features & Traits": {
-                "Racial Traits": {},
-                "Class Features": {}
-            },
-            "Inventory": {
-                "Equipment": {},
-                "Backpack": {},
-                "Treasures": {}
-            },
-            "Spellbook": {}
-        }
-    c.features["Features & Traits"]["Racial Traits"]["Darkvision"] = \
-        """Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can't discern color in darkness, only shades of gray."""
-    c.features["Features & Traits"]["Racial Traits"]["Gnome Cunning"] = \
-        """You have advantage on all Intelligence, Wisdom, and Charisma saving throws against magic."""
-    c.features["Features & Traits"]["Racial Traits"]["Artificers Lore"] = \
-        """Whenever you make an Intelligence (History) check related to magic items, alchemical objects, or technological devices, you can add twice your proficiency bonus, instead of any proficiency bonus you normally apply."""
-    c.features["Features & Traits"]["Racial Traits"]["Tinker"] = \
-        """You have proficiency with artisan's tools (tinker's tools). Using those tools, you can spend 1 hour and 10 gp worth of materials to construct a Tiny clockwork device (AC 5, 1 hp). The device ceases to function after 24 hours (unless you spend 1 hour repairing it to keep the device functioning), or when you use your action to dismantle it; at that time, you can reclaim the materials used to create it. You can have up to three such devices active at a time.
-When you create a device, choose one of the following options:
-
-Clockwork Toy.
-This toy is a clockwork animal, monster, or person, such as a frog, mouse, bird, dragon, or soldier. When placed on the ground, the toy moves 5 feet across the ground on each of your turns in a random direction. It makes noises as appropriate to the creature it represents.
-
-Fire Starter.
-The device produces a miniature flame, which you can use to light a candle, torch, or campfire. Using the device requires your action.
-
-Music Box.
-When opened, this music box plays a single song at a moderate volume. The box stops playing when it reaches the song's end or when it is closed.
-"""
-    c.features["Features & Traits"]["Class Features"]["Bardic Inspiration"] = \
-        """You can inspire others through stirring words or music. To do so, you use a bonus action on your turn to choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d6.
-
-Once within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, attack roll, or saving throw it makes. The creature can wait until after it rolls the d20 before deciding to use the Bardic Inspiration die, but must decide before the DM says whether the roll succeeds or fails. Once the Bardic Inspiration die is rolled, it is lost. A creature can have only one Bardic Inspiration die at a time.
-
-You can use this feature a number of times equal to your Charisma modifier (a minimum of once). You regain any expended uses when you finish a long rest.
-
-Your Bardic Inspiration die changes when you reach certain levels in this class. The die becomes a d8 at 5th level, a d10 at 10th level, and a d12 at 15th level."""
+    c.skill_proficiencies = ['Deception', 'Intimidation', 'Performance']
     c.features["Proficiencies"]["Weapons & Armour"]["Light Armour"] = \
         """Light armor is favored by bards because it lets them make the best use of their Dexterity and doesn't interfere with performances. When you wear light armor, you add your Dexterity modifier to the number shown on the table to determine your Armor Class."""
     c.features["Proficiencies"]["Weapons & Armour"]["Simple Weapons"] = \
@@ -99,7 +71,54 @@ Your Bardic Inspiration die changes when you reach certain levels in this class.
         """"""
     c.features["Proficiencies"]["Weapons & Armour"]["Shortswords"] = \
         """"""
+    c.features["Proficiencies"]["Tools"]["Lute"] = \
+        """"""
+    c.features["Proficiencies"]["Tools"]["Viol"] = \
+        """"""
+    c.features["Proficiencies"]["Tools"]["Flute"] = \
+        """"""
+    c.features["Features & Traits"]["Class Features"]["Bardic Inspiration"] = \
+        """You can inspire others through stirring words or music. To do so, you use a bonus action on your turn to choose one creature other than yourself within 60 feet of you who can hear you. That creature gains one Bardic Inspiration die, a d6.
 
+Once within the next 10 minutes, the creature can roll the die and add the number rolled to one ability check, attack roll, or saving throw it makes. The creature can wait until after it rolls the d20 before deciding to use the Bardic Inspiration die, but must decide before the DM says whether the roll succeeds or fails. Once the Bardic Inspiration die is rolled, it is lost. A creature can have only one Bardic Inspiration die at a time.
+
+You can use this feature a number of times equal to your Charisma modifier (a minimum of once). You regain any expended uses when you finish a long rest.
+
+Your Bardic Inspiration die changes when you reach certain levels in this class. The die becomes a d8 at 5th level, a d10 at 10th level, and a d12 at 15th level."""
+    c.features["Features & Traits"]["Class Features"]["Spellcasting"] = \
+        """You have learned to untangle and reshape the fabric of reality in harmony with your wishes and music. Your spells are part of your vast repertoire, magic that you can tune to different situations. See Spells Rules for the general rules of spellcasting and the Spells Listing for the bard spell list.
+
+        You can use a musical instrument (see the Tools section) as a spellcasting focus for your bard spells."""
+
+
+    c.background = "Stone Mason Guild Artisan"
+    c.skill_proficiencies += ['Insight', 'Perception']
+    c.features["Proficiencies"]["Tools"]["Mason's Tools"] = \
+    """"""
+    c.features["Proficiencies"]["Languages"]["Gnomish"] = """"""
+    c.features["Inventory"]["Equipment"] = tcs.Item()
+
+
+def sheet_layout(c):
+
+    c.features = \
+        {
+            "Proficiencies": {
+                "Weapons & Armour": {},
+                "Tools": {},
+                "Languages": {}
+            },
+            "Features & Traits": {
+                "Racial Traits": {},
+                "Class Features": {}
+            },
+            "Inventory": {
+                "Equipment": {},
+                "Backpack": {},
+                "Treasures": {}
+            },
+            "Spellbook": {}
+        }
 
 def mod(skill):
     return int((skill - 10) / 2)
