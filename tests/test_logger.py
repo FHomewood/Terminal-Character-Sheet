f0a96c04ce3531dc36233c7024e8fd3166a5a596
DVFrankieH <frankie.homewood@business-thinking.com>
2023-09-24 09:51:23 +0000

    Add additional tests for LoggingUtil

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -8,13 +8,16 @@ from terminal_character_sheet.logger import LoggingUtil
 
 
 def test_logging_util_initializes_path_object():
-    file_path = Path(__file__).parent / "log/test.log"
+    file_path = (Path(__file__).parent / "log/test_logging_util_initializes_path_object.log")
+    file_path.unlink(missing_ok=True)
     logger = LoggingUtil(file_path)
     assert hasattr(logger, "directory")
+    assert file_path.exists()
+
 
 def test_log_prints_to_file():
-    file_path = Path(__file__).parent / "log/test.log"
-    shutil.rmtree(file_path.parent, ignore_errors=True)
+    file_path = Path(__file__).parent / "log/test_log_prints_to_file.log"
+    file_path.unlink(missing_ok=True)
     logger = LoggingUtil(file_path)
 
     logger.log("test")
@@ -24,9 +27,10 @@ def test_log_prints_to_file():
 
     assert "test" in file_contents
 
-def test_seperate_log_call_append_to_file():
-    file_path = Path(__file__).parent / "log/test.log"
-    shutil.rmtree(file_path.parent, ignore_errors=True)
+
+def test_separate_log_call_append_to_file():
+    file_path = Path(__file__).parent / "log/test_separate_log_call_append_to_file.log"
+    file_path.unlink(missing_ok=True)
     logger = LoggingUtil(file_path)
 
     logger.log("test one")
@@ -38,3 +42,32 @@ def test_seperate_log_call_append_to_file():
 
     assert "test one" in file_contents
     assert "test two" in file_contents
+
+
+def test_log_level_defaults_to_info():
+    file_path = Path(__file__).parent / "log/test_log_level_defaults_to_info.log"
+    file_path.unlink(missing_ok=True)
+    logger = LoggingUtil(file_path)
+
+    logger.log("test")
+
+    with open(file_path) as file:
+        file_contents = ''.join(file.readlines())
+
+    assert "INFO | test" in file_contents
+
+
+def test_different_levels_register_correctly():
+    file_path = Path(__file__).parent / "log/test_different_levels_register_correctly.log"
+    file_path.unlink(missing_ok=True)
+    logger = LoggingUtil(file_path)
+
+    logger.log("test one", level="INFO")
+
+    logger.log("test two", level="DEBUG")
+
+    with open(file_path) as file:
+        file_contents = ''.join(file.readlines())
+
+    assert "INFO | test one" in file_contents
+    assert "DEBUG | test two" in file_contents
