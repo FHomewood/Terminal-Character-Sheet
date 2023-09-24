7b2ad8f342bd8efd2f5c9bc50e58af707bdd5c59
DVFrankieH <frankie.homewood@business-thinking.com>
2023-09-24 09:58:21 +0000

    Add class stubs to character class

--- terminal_character_sheet/character.py
+++ terminal_character_sheet/character.py
@@ -1,14 +1,18 @@
 # Copyright 2022 Frankie Homewood
 
 from terminal_character_sheet.inventory import Inventory
+from terminal_character_sheet.traits import Traits
+from terminal_character_sheet.actions import Actions
+from terminal_character_sheet.display import Display
+from terminal_character_sheet.input_handler import InputHandler
 
 class Character:
     """Facade Character class to communicate between different subsystems"""
 
     def __init__(self):
-        self.display = None
-        self.input_handler = None
+        self.display = Display()
+        self.input_handler = InputHandler()
 
-        self.traits = None
+        self.traits = Traits()
         self.inventory = Inventory()
-        self.actions = None
+        self.actions = Actions()
