acdaba9cdf6761e02c4b5026bd973948ac88b38c
DVFrankieH <frankie.homewood@business-thinking.com>
2023-09-25 15:21:02 +0000

    Implement more tests for Display

---
+++ terminal_character_sheet/display_module.py
@@ -0,0 +1,4 @@
+# Copyright 2022 Frankie Homewood
+
+class DisplayModule:
+    """Holds a module interactive module for the terminal display"""
--- tests/test_display.py
+++ tests/test_display.py
@@ -4,6 +4,7 @@ import pytest
 from unittest.mock import patch
 
 from terminal_character_sheet.display import Display
+from terminal_character_sheet.display_module import DisplayModule
 
 
 def test_display_initializes_sub_systems():
@@ -16,24 +17,35 @@ def test_display_initializes_sub_systems():
     assert hasattr(display, "modules")
 
 
-def test_mouse_position_property_calculates_correctly():
+def test_mouse_position_property():
     display = Display()
     display.mouse_state = (0, 1, 2, 3, 4)
 
     assert display.mouse_position == (1, 2)
 
 
-def test_display_getch_updates_mouse_state():
-    pass
+def test_mouse_button_property():
+    display = Display()
 
+    display.mouse_state = (0, 1, 2, 3, 4)
 
-def test_display_getch_doesnt_update_negative_mouse_states():
-    pass
+    assert display.mouse_button == "Left Click"
 
 
 def test_module_is_added_on_call():
-    pass
+    display = Display()
+    test_module = DisplayModule()
+
+    display.add_module("test_module", test_module)
+
+    assert "test_module" in display.modules
+    assert display.modules.get("test_module") == test_module
 
 
 def test_module_is_removed_on_call():
-    pass
+    display = Display()
+    display.modules = {"test_module": DisplayModule()}
+
+    display.del_module("test_module")
+
+    assert display.modules == {}
